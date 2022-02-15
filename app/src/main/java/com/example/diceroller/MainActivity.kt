770592bd0f5f1386f529e086c52b277d213a77ed
ThomasGiros <thomas.giros@gmail.com>
2022-02-15 11:44:37 -0500

    image of dice adapting and adding a second dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,20 +12,47 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            rollDice()
+            rollDices()
         }
+
+        // Do a dice roll when the app starts
+        rollDices()
+
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    private fun rollDices() {
         // Create new Dice object with 6 sides and roll it
-        val diceRoll = Dice(6).roll()
+        val dice1Roll = Dice(6).roll()
+        val dice2Roll = Dice(6).roll()
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage1: ImageView = findViewById(R.id.dice_num_1)
+        val diceImage2: ImageView = findViewById(R.id.dice_num_2)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        // Update the ImageView with the correct drawable resource ID
+        diceImage1.setImageResource(changeDrawableResource(dice1Roll))
+        diceImage2.setImageResource(changeDrawableResource(dice2Roll))
+
+        // Update the content description
+        diceImage1.contentDescription = dice1Roll.toString()
+        diceImage2.contentDescription = dice2Roll.toString()
+    }
+
+    private fun changeDrawableResource(diceRoll: Int) : Int {
+        val drawableResource = when (diceRoll)
+        {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableResource
     }
 
 
